8ed2c5e1672c1649a03078145f405797ada1c53c
Slava Petrov <slavik--petrov@yandex.ru>
2018-04-23 10:58:21 +0000

    Update and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,27 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'new'
+    end    
+  end
+
 
   def show
     @post = Post.find(params[:id])
@@ -16,6 +34,13 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
 private
